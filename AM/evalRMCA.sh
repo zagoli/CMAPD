df72e583d819b3ecf311f2dfc5d4c47e32341fd6
Jacopo Zagoli <jacopo.zagoli@studenti.univr.it>
2023-05-22 17:27:09 +0200

    moved RMCA in project

--- AM/evalRMCA.sh
+++ AM/evalRMCA.sh
@@ -7,7 +7,7 @@ do
 	map="instances/maps/$i.map"
 	agents="instances/maps/$i.map"
 	tasks="instances/tasks/$i.task"
-	cmdRMCA="../../MCA-RMCA/build/MAPD -m $map -a $agents -t $tasks -c 60 -s PP --capacity 3 --objective total-travel-delay --only-update-top --kiva --regret"
+	cmdRMCA="../MCA-RMCA/build/MAPD -m $map -a $agents -t $tasks -c 60 -s PP --capacity 3 --objective total-travel-delay --only-update-top --kiva --regret"
 	cmdModel="python eval.py -m $map -t $tasks -s models/transformer_conflicts_small.pth"
 	# Esecuzione del comando
 	# echo "INSTANCE $i --------------------------------------------------"
--- PBS/evaluate.sh
+++
@@ -1,3 +0,0 @@
-for i in $(seq 0 $(($4-1))); do
-    ./pbs -m env/grid.map -a instances/a$2-t$3/ta-$1/$i.assignment -o ../pbs_$1_a$2_t$3.csv -k $2 -t 60
-done
